4f21d742b57745fd1faeed80d9768bd037c22a49
Ahmed Abdelsamad <a.m.abdelsamed@gmail.com>
2024-04-23 09:59:47 +0200

    Update extract-ssl.pfx.sh

--- extract-ssl.pfx.sh
+++ extract-ssl.pfx.sh
@@ -9,3 +9,7 @@ openssl rsa -in [keyfile-encrypted.key] -out [keyfile-decrypted.key]
 #Export certificate file:
 
 openssl pkcs12 -in [yourfile.pfx] -clcerts -nokeys -out [certificate.crt]
+
+
+## Export csr from key
+openssl req -out .\bot_maan_gov_ae.csr -new -key .\bot_maan_gov_ae-decrypt.key
